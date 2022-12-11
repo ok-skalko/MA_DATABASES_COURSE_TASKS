92aa30d5fa85c023b8efede105840772c9371796
serhii.trokhymets <serhii.trokhymets@masterofcode.com>
2022-12-11 11:27:48 +0200

    Hometask 1

---
+++ HW1/HW1_SerhiiTrokhymets.sql
@@ -0,0 +1,41 @@
+-- Ex 1
+select * from sakila.customer;
+
+-- Ex 2
+select 
+  first_name as "First Name", 
+  last_name as "Last Name", 
+  email as "Email" 
+from sakila.customer;
+
+-- Ex 3 desc спадання
+select
+  address as "Addres",
+  district as "District",
+  postal_code as "Postal Code"
+from sakila.address
+order by district, address desc;
+
+-- Ex 4
+select * from sakila.film
+  where rental_rate > 3;
+  
+--   Ex 5
+select 
+  title as "Title",
+  description as "Description",
+  rating as "Rating"
+from sakila.film
+  where rating in ("G", "PG", "R"); 
+  
+--   Ex 6
+select * from sakila.film
+  where rental_duration = 3 and replacement_cost < 12;
+  
+--   Ex 7
+select * from sakila.film
+  where rating = "G" and replacement_cost > 15;
+  
+--   Ex 8
+select * from sakila.film
+where length between 60 and 90;
