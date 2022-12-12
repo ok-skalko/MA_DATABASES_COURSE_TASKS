c65644d1e35064a4426a7d2a59a347cc423d4dfd
KattyKey <little.killer.93.11@gmail.com>
2022-12-12 15:30:46 +0200

    Add files via upload

---
+++ HW1/HW1_KaterynaNovosol.sql
@@ -0,0 +1,45 @@
+-- Task 1
+select * from sakila.customer;
+
+-- Task 2
+select first_name as "First Name"
+, last_name as "Last Name"
+, email as "Email"
+from sakila.customer ;
+
+-- Task 3
+select address as "Address"
+, district as "District"
+, postal_code as "Postal Code"
+from sakila.address
+order by district asc, address desc;
+
+-- Task 4 
+ select title
+, rental_rate
+from sakila.film
+where rental_rate > 3;
+
+-- Task 5
+select title
+, description
+,rating
+from sakila.film
+where rating in ("G", "PG" ,"R");
+
+-- Task 6
+select *
+from sakila.film
+where rental_duration = 3 and replacement_cost < 12;
+
+-- Task 7
+ select *
+from sakila.film
+where rating = "G" and replacement_cost > 15;
+
+-- Task 8
+ select *
+from sakila.film
+where length between 60 and 90;
+
+
