46c7aa70d26366949c7f833bb81fff5d1726f520
TArch64 <tarasturchenko1551@gmail.com>
2022-12-17 13:12:18 +0200

    complete task 5

--- HW2/HW2_TarasTurchenko.sql
+++ HW2/HW2_TarasTurchenko.sql
@@ -101,3 +101,32 @@ where customer_id in (
     select r.customer_id from rental as r
     where r.return_date is null
 );
+
+
+
+-- TASK 5
+-- Вивести всі фільми, видані в прокат менеджером Mike Hillyer.
+-- Для визначення менеджера використати таблицю staff і поле staff_id;
+-- для визначення фільму скористатися таблицею inventory (поле inventory_id),
+-- і таблиці film (поле film_id).
+
+-- JOIN
+select distinct f.film_id as 'ID', f.title as 'Film' from rental as r
+left join inventory i on r.inventory_id = i.inventory_id
+left join film f on i.film_id = f.film_id
+left join staff s on r.staff_id = s.staff_id
+where s.first_name = 'Mike' and s.last_name = 'Hillyer'
+order by f.film_id;
+
+-- SUB-QUERY
+select f.film_id as 'ID', f.title as 'Film' from film as f
+where f.film_id in (
+    select i.film_id from inventory as i
+    where i.inventory_id in (
+        select r.inventory_id from rental as r
+        where r.staff_id in (
+            select s.staff_id from staff as s
+            where s.first_name = 'Mike' and s.last_name = 'Hillyer'
+        )
+    )
+);
