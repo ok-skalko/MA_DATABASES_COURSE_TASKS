352b9d0ab9452c392b3e1ba483169719f6f6dc94
Volodymyr Horb <volodyahorb@gmail.com>
2022-12-18 20:54:52 +0200

    added 2 competed tasks

--- HW2/HW2_VolodymyrHorb.sql
+++ HW2/HW2_VolodymyrHorb.sql
@@ -1,12 +1,15 @@
 -- TASK  1;
-
-
+SELECT 
+address,
+(select city from sakila.city as sc where sc.city_id = sa.city_id) as City
+from address as sa
+where  sa.postal_code = '' or sa.phone = ''
 
 --v2 join
 SELECT address, city
  FROM sakila.address as sa
 join sakila.city as sc on sc.city_id=sa.city_id
-where  address2 is null
+where  postal_code = '' or phone = ''
 
 -- TASK  2;
 select 
@@ -102,7 +105,48 @@ on sc.customer_id = sr.customer_id
 where return_date is null;
 
 -- TASK 5;
-
+SELECT 
+  title as Film
+from 
+  sakila.film 
+where 
+  film_id in (
+    SELECT 
+      distinct film_id 
+    FROM 
+      sakila.inventory as si 
+    where 
+      inventory_id in (
+        SELECT 
+          inventory_id 
+        from 
+          sakila.rental as sr 
+        where 
+          staff_id = (
+            SELECT 
+              staff_id 
+            from 
+              sakila.staff as ss 
+            where 
+              ss.first_name = "Mike" 
+              and ss.last_name = "Hillyer"
+          )
+      )
+  )
+order by title asc;
+--v2 join
+select 
+  distinct title as Film
+from 
+  sakila.film as sf 
+  join sakila.inventory as si on si.film_id = sf.film_id 
+  join sakila.rental as sr on sr.inventory_id = si.inventory_id 
+  join sakila.staff as ss on ss.staff_id = sr.staff_id 
+where 
+  ss.first_name = "Mike" 
+  and ss.last_name = "Hillyer" 
+order by 
+  title asc
 
 
 -- TASK 6;
